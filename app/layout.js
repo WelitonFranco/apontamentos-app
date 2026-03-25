ccd539b91c6fa9d997afcf01ef94f6809cf46e1e
Weliton Franco <88354710+WelitonFranco@users.noreply.github.com>
2026-03-25 17:15:33 +0000

    ativa tailwind

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,13 @@
 import "./globals.css";
 
 export const metadata = {
-  title: 'Apontamentos'
-}
+  title: "Apontamentos",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body>{children}</body>
     </html>
-  )
+  );
 }
